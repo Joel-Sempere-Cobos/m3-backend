1b3a0edeb88072fb0e0f614bb55c62bd97e55be3
Joel-Sempere-Cobos <91223315+Joel-Sempere-Cobos@users.noreply.github.com>
2022-09-13 23:16:40 +0200

    revisar Error undefined

--- ejemplos/ejemplo_express/index.js
+++ ejemplos/ejemplo_express/index.js
@@ -9,29 +9,29 @@ const app = express();
 
 //crear ruta
 app.use((req, res, next) => {
-    console.log("He recibido una petición de tipo", req.method, "a", req.path);
-    next();
+  console.log("He recibido una petición de tipo", req.method, "a", req.path);
+  next();
 });
 
 app.get("/", (req, res, next) => {
-    if (true) {
-        error = new Error("El servidor está caído");
-        next(error);
-        return;
-    }
-    res.send("hola");
+  res.send("hola");
 });
 
 app.get("/pedidos", (req, res, next) => {
-    res.send("hola desde pedidos");
+  if (true) {
+    error = new Error("El servidor está caído");
+    next(error);
+    return;
+  }
+  res.send("hola desde pedidos");
 });
 
 app.use((err, req, res, next) => {
-    res.send("Ocurrió un error: " + err.message);
+  res.send("Ocurrió un error: " + err.message);
 });
 
 //arrancar la aplicación
 
 app.listen(8080, () => {
-    console.log("Servidor http arrancado en http://localhost:8080");
+  console.log("Servidor http arrancado en http://localhost:8080");
 });
